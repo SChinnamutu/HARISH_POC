07d29c850e17cd8322c18f6849ca1277166ab955
Sasi <mech.sasi82@gmail.com>
2019-12-18 16:00:12 +0530

    Removed

--- src/main/java/com/mufg/alert/service/PutSevMessageToQService.java
+++
@@ -1,9 +0,0 @@
-package com.mufg.alert.service;
-
-import com.ibm.mq.MQException;
-import com.ibm.mq.MQMessage;
-
-public interface PutSevMessageToQService {
-
-	public void putMessage(String qMgrName, String qName, MQMessage pMsg) throws MQException;
-}
